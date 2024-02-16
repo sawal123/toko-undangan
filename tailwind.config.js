e5d746157627db65024789ddc9d2acf6ee304968
sawal123 <sawalinto@gmail.com>
2024-02-16 23:36:49 +0700

    accordion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["*.html"],
+  
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 }
