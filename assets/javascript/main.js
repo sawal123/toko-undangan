d3fb1410a039996ecf8be210e9bd5d7f32c6d34a
sawal123 <sawalinto@gmail.com>
2024-02-18 23:44:50 +0700

    Fix accordion

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,26 +1,14 @@
-var accor = document.getElementById('accor')
-var ac = document.getElementById('ac')
-var subAc = document.getElementById('subAc')
-var arrow = document.getElementById('arrow')
 
-var elements = document.querySelectorAll('[id^="ac"]');
-elements.forEach(function(element){
-    console.log(elements)
-})
+var elements = document.querySelectorAll('.ac');
+elements.forEach(element =>{
+    element.addEventListener('click',()=>{
+        const icon = element.querySelector('i')
+    const subAc = element.nextElementSibling;
+    console.log(element)
+    subAc.classList.toggle('hidden')
+    icon.classList.toggle('rotate-90')
+    
 
-elements.addEventListener('click', function(){
-    if(subAc.classList.contains('hidden') ){
-        subAc.classList.remove('hidden')
-        subAc.classList.remove('opacity-0')
-        subAc.classList.add('opacity-100')
-        arrow.classList.add('rotate-90')
 
-        
-    }else{
-        subAc.classList.add('hidden')
-        subAc.classList.remove('opacity-100')
-        subAc.classList.add('opacity-100')
-        arrow.classList.remove('rotate-90')
-    }
-     
-})
+    })
+});
