3363494dc933fbe42f94022014f7edb6265ae881
sawal123 <sawalinto@gmail.com>
2024-02-28 21:38:33 +0700

    page detail

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -26,19 +26,74 @@ $(document).ready(function () {
   });
 });
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:2
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
+
+$(".category").owlCarousel({
+  loop: true,
+  margin: 10,
+  autoWidth: true,
+  nav: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
+
+$('.header-undangan').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 1,
+  // autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
     }
-})
+  ]
+});
+
+
+function myImg(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
